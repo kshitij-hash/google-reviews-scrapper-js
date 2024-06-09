e1708a872f8c2c0e78661475f1cc5ad5d639ce3d
kshitij-hash <83551355+kshitij-hash@users.noreply.github.com>
2024-06-10 02:51:01 +0530

    updated args for running on ubuntu machine

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.post('/scrape', async (req, res) => {
 
     try {
         const browser = await puppeteer.launch({
-            headless: false,
-            defaultViewport: null
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
         await page.goto(url, {
